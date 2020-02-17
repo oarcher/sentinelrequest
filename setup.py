a44a4e68887d66d47861555ad97b3a4ae97a2cc7
Olivier Archer <olivier.archer@ifremer.fr>
2020-02-17 19:13:13 +0100

    version from setuptools_scm

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 setup(name='sentinelRequest',
       description='scihub and peps requests from command line and python',
       url='https://gitlab.ifremer.fr/sarwing/sentinelrequest.git',
-      version = "0.0.1",
       author = "Olivier Archer",
       author_email = "Olivier.Archer@ifremer.fr",
       license='GPL',
       packages=['sentinelRequest'],
+      use_scm_version={'version_scheme' : 'python-simplified-semver'},
+      setup_requires=['setuptools_scm'],
       zip_safe=False,
       scripts=['bin/sentinelRequest'],
       install_requires=[ 'geopandas', 'requests',  'lxml',  'fiona' , 'html2text', 'geo_shapely @ git+https://gitlab.ifremer.fr/oa04eb3/geo_shapely.git', 'geopandas_coloc @ git+https://gitlab.ifremer.fr/oa04eb3/geopandas_coloc.git', 'tqdm' ]
